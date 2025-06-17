d846108702b70b621b704b50a9f29aab60e2b78f
janisaiad <janisaiad.ja@gmail.com>
2025-06-17 10:40:09 +0200

    before implementing the rewriting system

--- model/finitewidth/finitewidth.py
+++ model/finitewidth/finitewidth.py
@@ -1,9 +1,33 @@
 import jax.numpy as jnp
+from dataclasses import dataclass
+from typing import List, Dict, Any, Optional, Callable, Union, Tuple, Literal
 
-class Kernel:
-    def __init__(self, n_entries: int, dim_input: int, entry_vectors: np.ndarray):
-        "The Kernel class is a container for the finite width corrections kernels"
+
+
+@dataclass
+class Term: # each term is a function of the entries
+    def __init__(self, entry_vectors: jnp.ndarray):
+        self.entry_vectors = entry_vectors # (dim_input, n_entries) in column format
+        self.weights = None # the weights of the term, as a jax.numpy array
+
+    def evolve(self,x_beta: jnp.ndarray) -> 'Term':
+        """we evolve the term with the formal system evolution law"""
+        entry_vectors_new = jnp.concatenate([self.entry_vectors, x_beta], axis=1) # we concat the entry vectors and the x_beta
         
+        ## to fill
+        return Term(self.entry_vectors_new)
+    
+    
+    
+    
+    
+    
+    
+class Kernel:
+    def __init__(self, n_entries: int, dim_input: int, entry_vectors: jnp.ndarray):
+        """The Kernel class is a container for the finite width corrections kernels
+            The number of layer can be changed, and this is the most interesting part
+            Same for the width"""        
         """
         Small description of what the class is doing:
         - If you have a NN, you can compute the Kernel at any point
@@ -22,19 +46,37 @@ class Kernel:
         self.entry_vectors = entry_vectors # (dim_input, n_entries) in column format
         self.terms = [] # list of terms in the kernel
         self.dataset = None # dataset over the sphere, R^d, the Torus.
+        
+        
+        
         self.NN = None # the NN stored, as a jax.nn.Dense object
         self.weights = None # the weights of the NN, as a jax.numpy array
-        
+        self.feature_maps = None # the feature maps of the NN, as a jax.numpy array
+        self.feature_maps_derivative = None # the derivative of the feature maps of the NN, as a jax.numpy array
 
-    def compute_kernel(self): # formula to implement
+    def compute_kernel(self,x: jnp.ndarray): # formula to implement
         pass
-
-    def compute_kernel_from_weights(self):
+    
+    def generate_NN(self):
         pass
 
     def evolve(self): # we apply the evolution law for each term in the kernel
+        terms = []
+        for term in self.terms:
+            terms.append(term.evolve())
+        return terms
+
+    def get_from_son(self): # we get the kernel from a son kernel
         pass
 
-    def evaluate(self):
+    def compute_statistics(self):
         pass
 
+    def generate_dataset(self):
+        pass
+    
+    def transfer_to_parent(self):
+        terms = self.evolve()
+        
+    def time_evolution(self):
+        pass
