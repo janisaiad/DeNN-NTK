60f18c716f1afb0250b8de1636253895e5e9678e
janisaiad <janisaiad.ja@gmail.com>
2025-06-29 19:00:20 +0200

    killed python rn

--- experiments/ntk_spectrum/eigen_distrib_largest_eigenvector.py
+++ experiments/ntk_spectrum/eigen_distrib_largest_eigenvector.py
@@ -68,7 +68,9 @@ def plot_largest_eigenvector_scalar_product_distribution(eigenvectors, N, D_IN, 
     """
     # we assume the largest eigenvector is at index 0
     # eigenvectors shape: (n_experiments, n_vectors, dimension)
-    eigenvectors = eigenvectors[:, -1, :]
+    for i in range(eigenvectors.shape[0]):
+        eigenvectors[i,:,:] = eigenvectors[i,:,:].T
+    
     try:
         largest_eigenvectors = eigenvectors[:, -1, :] # shape (n_experiments, dimension)
         print(largest_eigenvectors)
